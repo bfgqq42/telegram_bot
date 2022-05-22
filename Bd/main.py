5a6bf254f01d38b78e5abc1adefd6d3ff80e62e5
bfgqq42 <one-partner-mk@my.com>
2022-05-22 21:19:44 +0300

    Изменения - стартовая панель с кнопками - клавиатуры статически генерируемы

--- Bd/main.py
+++ Bd/main.py
@@ -11,6 +11,15 @@ def issued(reason, text):
     print(f"> Issued {reason} for \'{text}\'" if text else "Command is None")
 
 
+def default_button(text, call):
+    """
+    Function gives button with callback.
+
+    :return: InlineKeyboardButton
+    """
+    return types.InlineKeyboardButton(text=text, callback_data=call)
+
+
 def create_button(folder, text):
     """
     Function gives button with custom callback.
@@ -26,7 +35,7 @@ def create_button_array(folder, num):
 
     :return: list of InlineKeyboardButton
     """
-    return [create_button(folder, i+1) for i in range(num)]
+    return [create_button(folder, i + 1) for i in range(num)]
 
 
 def fill_keyboard(buttons, width):
@@ -45,6 +54,72 @@ def callback(call):
         with open(f'data/{folder}/{file}.txt', 'r', encoding='utf-8') as f:
             bot.send_message(call.message.chat.id, f.read())
         issued("callback", call.data)
+    elif 'basic' in call.data:
+        text = call.data.split('-')[1]
+        if text == 'bach':
+            bot.send_message(call.message.chat.id, 'Нажми кнопку с цифрой интересующего тебя вопроса:\n\n'
+                                                   '1. Какой срок обучения?\n'
+                                                   '2. Какая стоимость обучения?\n'
+                                                   '3. Есть ли бюджетные места?\n'
+                                                   '4. Какие вступительные экзамены необходимы для поступления?\n'
+                                                   '5. Какие документы необходимо подать в вуз?\n'
+                                                   '6. Какие экзамены нужны для поступления на ЦифрЭк?\n'
+                                                   '7. Предоставляется ли общежитие?\n'
+                                                   '8. Какой проходной бал при поступлении?\n'
+                                                   '9. Как и когда можно заключить договор?\n'
+                                                   '10. Есть ли военная кафедра и как на нее поступить?\n\n'
+                                                   'Eсли хочешь оставить заявку чтобы тебе перезвонили по данному вопросу напиши /reg_bach\n',
+                             reply_markup=bach_keyboard)
+
+        elif text == 'magi':
+            bot.send_message(call.message.chat.id, 'Нажми кнопку с цифрой интересующего тебя вопроса:\n\n'
+                                                   '1. Какой срок обучения?\n'
+                                                   '2. Какая стоимость обучения?\n'
+                                                   '3. Есть ли бюджетные места?\n'
+                                                   '4. Военная кафедра\n'
+                                                   '5. Проходной бал при поступлении\n'
+                                                   '6. Какие экзамены?\n'
+                                                   '7. Общежитие'
+                                                   '8. Подробнее о программе\n'
+                                                   '9. Как и когда можно заключить договор?\n'
+                                                   '10. Другое. Напиши свой вопрос\n\n'
+
+                                                   'Eсли хочешь оставить заявку чтобы тебе перезвонили по данному вопросу напиши /reg_magi\n',
+                             reply_markup=magi_keyboard)
+
+        elif text == 'auxi':
+            bot.send_message(call.message.chat.id, 'Нажми кнопку с цифрой интересующего тебя вопроса:\n\n'
+                                                   '1. International course "Digital economy & AgriFoodTech" Bachelor’s Degree '
+                                                   '(на английском языке)\n'
+                                                   '2. Дополнительное образование Цифровая Экономика в АПК (252 часа)\n'
+                                                   '3. MBA Executive\n'
+                                                   '4. Startup-студия. Центр инновационного предпринимательства.\n'
+                                                   '5. Консалтинг, индивидуальные консультации\n'
+                                                   '6. Хочу стать партнёром ЦифрЭк\n'
+                                                   '7. Контакты'
+                                                   '8. Попасть на сайт ЦифрЭк\n'
+                                                   '9. Как добраться в ЦифрЭк\n'
+                                                   '10. Другое. Задать свой вопрос.\n\n'
+
+                                                   'Eсли хочешь оставить заявку чтобы тебе перезвонили по данному вопросу напиши /reg_auxi\n',
+                             reply_markup=auxi_keyboard)
+        else:
+            bot.send_message(call.message.chat.id, 'Простите, я вас не понимаю. Напишите /help')
+
+
+help_keyboard = types.InlineKeyboardMarkup(row_width=1).add(*[
+    default_button('Бакалавр',        'basic-bach'),
+    default_button('Магистратура',    'basic-magi'),
+    default_button('Доп. информация', 'basic-auxi')
+])
+
+rows = 3
+bach_questions = 10
+bach_keyboard = fill_keyboard(create_button_array('bach', bach_questions), rows)
+magi_questions = 10
+magi_keyboard = fill_keyboard(create_button_array('magi', magi_questions), rows)
+auxi_questions = 10
+auxi_keyboard = fill_keyboard(create_button_array('auxi', auxi_questions), rows)
 
 
 @bot.message_handler(content_types=['text'])
@@ -54,71 +129,11 @@ def start(message):
     issued("command", message.text)
 
     if text in ['/help', 'Здравствуй', 'Здарова', 'Добрый день']:
-        bot.send_message(user, 'Выбери одно из интересующих тебя направлений: \n'
-                               '1. Бакалавр - /bachelor\n'
-                               '2. Магистратура - /magistracy\n'
-                               '3. Доп. информация - /aux_info')
-
-    elif text == '/bachelor':
-        buttons = create_button_array('bach', 11)
-        keyboard = fill_keyboard(buttons, 3)
-        bot.send_message(user, 'Нажми кнопку с цифрой интересующего тебя вопроса:\n\n'
-                               '1. Какой срок обучения?\n'
-                               '2. Какая стоимость обучения?\n'
-                               '3. Есть ли бюджетные места?\n'
-                               '4. Какие вступительные экзамены необходимы для поступления?\n'
-                               '5. Какие документы необходимо подать в вуз?\n'
-                               '6. Какие экзамены нужны для поступления на ЦифрЭк?\n'
-                               '7. -\n'
-                               '8. Предоставляется ли общежитие?\n'
-                               '9. Какой проходной бал при поступлении?\n'
-                               '10. Как и когда можно заключить договор?\n'
-                               '11. Есть ли военная кафедра и как на нее поступить?\n\n'
-                         'Eсли хочешь оставить заявку чтобы тебе перезвонили по данному вопросу напиши /reg_bach\n',
-                         reply_markup=keyboard)
-
-    elif text == '/magistracy':
-        buttons = create_button_array('magi', 10)
-        keyboard = fill_keyboard(buttons, 3)
-        bot.send_message(user, 'Нажми кнопку с цифрой интересующего тебя вопроса:\n\n'
-                               '1. Какой срок обучения?\n'
-                               '2. Какая стоимость обучения?\n'
-                               '3. Есть ли бюджетные места?\n'
-                               '4. Военная кафедра\n'
-                               '5. Проходной бал при поступлении\n'
-                               '6. Какие экзамены?\n'
-                               '7. Общежитие'
-                               '8. Подробнее о программе\n'
-                               '9. Как и когда можно заключить договор?\n'
-                               '10. Другое. Напиши свой вопрос\n\n'
-            
-                         'Eсли хочешь оставить заявку чтобы тебе перезвонили по данному вопросу напиши /reg_magi\n',
-                         reply_markup=keyboard)
-
-    elif text == '/aux_info':
-        buttons = create_button_array('aux', 10)
-        keyboard = fill_keyboard(buttons, 3)
-        bot.send_message(user, 'Нажми кнопку с цифрой интересующего тебя вопроса:\n\n'
-                               '1. International course "Digital economy & AgriFoodTech" Bachelor’s Degree '
-                               '(на английском языке)\n'
-                               '2. Дополнительное образование Цифровая Экономика в АПК (252 часа)\n'
-                               '3. MBA Executive\n'
-                               '4. Startup-студия. Центр инновационного предпринимательства.\n'
-                               '5. Консалтинг, индивидуальные консультации\n'
-                               '6. Хочу стать партнёром ЦифрЭк\n'
-                               '7. Контакты'
-                               '8. Попасть на сайт ЦифрЭк\n'
-                               '9. Как добраться в ЦифрЭк\n'
-                               '10. Другое. Задать свой вопрос.\n\n'
-
-                               'Eсли хочешь оставить заявку чтобы тебе перезвонили по данному вопросу напиши /reg_aux\n',
-                         reply_markup=keyboard)
-
+        bot.send_message(user, 'Выбери одно из интересующих тебя направлений:', reply_markup=help_keyboard)
     elif '/reg' in text and len(text) > 5:
         dbname = text.split('_')[1]
-        if dbname in ['bach', 'magi', 'aux']:
+        if dbname in ['bach', 'magi', 'auxi']:
             Register(bot, message, dbname)
-
     else:
         bot.send_message(user, 'Простите, я вас не понимаю. Напишите /help')
 
