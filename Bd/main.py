da307923f708b163d27323fd28b8dfbd2e765c51
bfgqq42 <one-partner-mk@my.com>
2022-06-23 10:32:45 +0300

    Изменения: Текст

--- Bd/main.py
+++ Bd/main.py
@@ -4,42 +4,32 @@ from Bd.presets.keyboards import *
 from Bd.libraries.lib import *
 
 from data.json_work import read
-from Bd.libraries.registration import *
 
 
 # bot configuration
-token = '5421671179:AAFLiNjDx55fJznLjLDn-g8xZaC6bzoF8t4'
+token = '5428301440:AAGACq5BLVyew5mnPpi-pGSCtqIUHF1t99E'
 bot: telebot.TeleBot = telebot.TeleBot(token)
 
 
 # shortcuts
-main_text = 'Eсли хочешь оставить заявку, чтобы тебе позвонили по интересующему тебя вопросу нажми на кнопку Зарегистрироваться'
+main_text = 'Выберете интересующий Вас вопрос:'
 
 shortcut = MultiDict(
-    ['eshko', 'ЭШКО Young'],
-    ['bach', 'Бакалавриат'],
+    ['bach', 'Бакалавриат/Специалитет'],
     ['magi', 'Магистратура'],
-    ['inter', 'International course (на английском языке)'],
-    ['dpo', 'ДПО ЦифрЭк в АПК (252 часа)'],
-    ['drugoe', 'Другое']
+    ['inter', 'Аспирантура']
 )
 
 all_keyboards = {
-    'eshko': eshko_keyboard,
     'bach': bach_keyboard,
     'magi': magi_keyboard,
-    'inter': inter_keyboard,
-    'dpo': dpo_keyboard,
-    'drugoe': drugoe_keyboard
+    'inter': inter_keyboard
 }
 
 ask_answers = {
-    'eshko': read('eshko'),
     'bach': read('bach'),
     'magi': read('magi'),
-    'inter': read('inter'),
-    'dpo': read('dpo'),
-    'drugoe': read('drugoe')
+    'inter': read('inter')
 }
 
 
@@ -53,7 +43,7 @@ def callback(call):
         text = search_ask(all_keyboards, call.data)
 
         bot.send_message(chat_id=call.message.chat.id,
-                         text=f'<b>{text}</b>\n' + ask_answers[spec][name],
+                         text=f'<b><>{text}</b>\n' + ask_answers[spec][name],
                          reply_markup=types.InlineKeyboardMarkup(row_width=1).add(*[
                              create_button('Вернуться назад', f'return-{spec}')
                          ]),
@@ -66,20 +56,6 @@ def callback(call):
                          text=f'{format_text(shortcut.get_value(text, 0), "uib")}\n' + main_text,
                          reply_markup=all_keyboards[text], parse_mode='HTML')
 
-    elif 'reg' in call.data:
-        dbname = call.data.split('-')[1]
-        if dbname == 'eshko':
-            RegisterEshko(bot, call.message, call.from_user.id, dbname)
-        elif dbname == 'bach':
-            RegisterBachMagi(bot, call.message, call.from_user.id, dbname)
-        elif dbname == 'magi':
-            RegisterBachMagi(bot, call.message, call.from_user.id, dbname)
-        elif dbname == 'inter':
-            RegisterInter(bot, call.message, call.from_user.id, dbname)
-        elif dbname == 'dpo':
-            RegisterDPO(bot, call.message, call.from_user.id, dbname)
-        elif dbname == 'drugoe':
-            RegisterDrugoe(bot, call.message, call.from_user.id, dbname)
 
 
 @bot.message_handler(content_types=['text'])
@@ -88,7 +64,7 @@ def start(message):
 
     if message.text in ['/help', '/start', 'Здравствуй', 'Здарова', 'Добрый день']:
         bot.send_message(chat_id=message.from_user.id,
-                         text='Выбери одно из интересующих тебя направлений',
+                         text='Выбери одно из интересующих тебя направлений:',
                          reply_markup=help_keyboard)
 
     elif message.text in shortcut.get_data(1):
--- Bd/presets/keyboards.py
+++ Bd/presets/keyboards.py
@@ -3,83 +3,29 @@ from Bd.libraries.lib import create_button
 
 
 # keyboards
-help_keyboard = types.ReplyKeyboardMarkup(row_width=1).add(*[
-    create_button('ЭШКО Young', 'basic-eshko'),
-    create_button('Бакалавриат', 'basic-bach'),
+help_keyboard = types.ReplyKeyboardMarkup(row_width=1).add(*{
+    create_button('Бакалавриат/Специалитет', 'basic-bach'),
     create_button('Магистратура', 'basic-magi'),
-    create_button('International course (на английском языке)', 'basic-inter'),
-    create_button('ДПО ЦифрЭк в АПК (252 часа)', 'basic-dpo'),
-    create_button('Другое', 'basic-drugoe'),
-
-])
-
-eshko_keyboard = types.InlineKeyboardMarkup(row_width=1).add(*[
-    create_button('1. Какой срок обучения?', 'eshko-ask_1'),
-    create_button('2. Какая стоимость обучения?', 'eshko-ask_2'),
-    create_button('3. Какой формат обучения?', 'eshko-ask_3'),
-    create_button('4. Подробнее об ЭШКО', 'eshko-ask_4'),
-
-    create_button('Зарегистрироваться', 'reg-eshko'),
-])
+    create_button('Аспирантура', 'basic-inter')
+})
 
 bach_keyboard = types.InlineKeyboardMarkup(row_width=1).add(*[
-    create_button('1. Какой срок обучения?', 'bach-ask_1'),
-    create_button('2. Какая стоимость обучения?', 'bach-ask_2'),
-    create_button('3. Есть ли бюджетные места?', 'bach-ask_3'),
-    create_button('4. Какие экзамены необходимы для поступления?', 'bach-ask_4'),
-    create_button('5. Какие документы необходимо подать в вуз?', 'bach-ask_5'),
-    create_button('6. Предоставляется ли общежитие?', 'bach-ask_6'),
-    create_button('7. Какой проходной бал при поступлении?', 'bach-ask_7'),
-    create_button('8. Как и когда можно заключить договор?', 'bach-ask_8'),
-    create_button('9. Есть ли военная кафедра и как на нее поступить?', 'bach-ask_9'),
-
-    create_button('Зарегистрироваться', 'reg-bach'),
+    create_button('1. Какие документы необходимы для подачи?', 'bach-ask_1'),
+    create_button('2. На сколько направлений Я могу подать документы?', 'bach-ask_2'),
+    create_button('3. Что такое очна-заочная форма обучения?', 'bach-ask_3'),
+    create_button('4. Перевод из другого ВУЗа', 'bach-ask_4'),
+    create_button('5. Кто может сдавать внутренние вступительные испытания?', 'bach-ask_5'),
+    create_button('6. Особая квота', 'bach-ask_6'),
+    create_button('7. Справка 086-У', 'bach-ask_7'),
 ])
 
 magi_keyboard = types.InlineKeyboardMarkup(row_width=1).add(*[
-    create_button('1. Какой срок обучения?', 'magi-ask_1'),
-    create_button('2. Какая стоимость обучения?', 'magi-ask_2'),
-    create_button('3. Есть ли бюджетные места?', 'magi-ask_3'),
-    create_button('4. Есть ли военная кафедра?', 'magi-ask_4'),
-    create_button('5. Какой проходной бал при поступлении?', 'magi-ask_5'),
-    create_button('6. Какие экзамены необходимы для сдачи?', 'magi-ask_6'),
-    create_button('7. Предоставляется ли общежитие?', 'magi-ask_7'),
-    create_button('8. Как и когда можно заключить договор?', 'magi-ask_8'),
-    create_button('9. Подробнее о программе', 'magi-ask_9'),
-
-    create_button('Обратная связь', 'reg-magi'),
+    create_button('1. Какие документы необходимы для подачи?', 'magi-ask_1'),
+    create_button('2. На сколько направлений Я могу подать документы?', 'magi-ask_2'),
+    create_button('3. Перевод из другого ВУЗа', 'magi-ask_3'),
+    create_button('4. Справка 086-У', 'magi-ask_4'),
+    create_button('5. Внутренние вступительные испытания', 'magi-ask_5'),
 ])
-
 inter_keyboard = types.InlineKeyboardMarkup(row_width=1).add(*[
-    create_button('1. Duration of training', 'inter-ask_1'),
-    create_button('2. Tuition fees', 'inter-ask_2'),
-    create_button('3. Budget places', 'inter-ask_3'),
-    create_button('4. Military Department', 'inter-ask_4'),
-    create_button('5. Entrance tests', 'inter-ask_5'),
-    create_button('6. Hostel', 'inter-ask_6'),
-    create_button('7. More about the program', 'inter-ask_7'),
-    create_button('8. How and when to conclude a contract?', 'inter-ask_8'),
-    create_button('9. Other. Write your question.', 'inter-ask_9'),
-
-    create_button('Register ', 'reg-inter'),
-])
-
-dpo_keyboard = types.InlineKeyboardMarkup(row_width=1).add(*[
-    create_button('1. Срок обучения?', 'dpo-ask_1'),
-    create_button('2. Блоки преподаваемых дисциплин', 'dpo-ask_2'),
-    create_button('3. Стоимость обучения', 'dpo-ask_3'),
-    create_button('4. Подробнее о программе', 'dpo-ask_4'),
-
-    create_button('Зарегистрироваться', 'reg-dpo'),
-])
-
-
-drugoe_keyboard = types.InlineKeyboardMarkup(row_width=1).add(*[
-    create_button('1. Наш сайт', 'drugoe-ask_1'),
-    create_button('2. Как добраться в ЦифрЭк?', 'drugoe-ask_2'),
-    create_button('3. Хочу стать партнёром ЦифрЭк', 'drugoe-ask_3'),
-    create_button('4. MBA/Программа для продвинутых', 'drugoe-ask_4'),
-    create_button('5. Преврати свою идею в прибыльный бизнес!', 'drugoe-ask_5'),
-    create_button('6. Консалтинговые услуги ЦифрЭк КубГАУ', 'drugoe-ask_6'),
-    create_button('7. Наши контакы', 'drugoe-ask_7'),
+    create_button('1. Подробнее об уровне образования', 'inter-ask_1'),
 ])
